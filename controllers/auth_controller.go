a3170d258ace9c79d630c0a68d6051dc1b985bc3
henriquelazzarino <henrique.a.lazzarino@gmail.com>
2024-06-15 10:13:52 -0300

    Start auth functions

---
+++ controllers/auth_controller.go
@@ -0,0 +1,10 @@
+package controllers
+
+import (
+    "net/http"
+    "github.com/henriquelazzarino/gookshelf/models"
+	"github.com/henriquelazzarino/gookshelf/services"
+    "github.com/gin-gonic/gin"
+)
+
+func Login(){}
--- models/user.go
+++ models/user.go
@@ -3,6 +3,8 @@ package models
 type User struct {
     ID           string `json:"id"`
     Name         string `json:"name"`
+    Email        string `json:"email"`
+    Password     string `json:"password"`
     Picture      string `json:"picture"`
     Age          int    `json:"age"`
     BookedBooks  []Book `json:"bookedBooks"`
